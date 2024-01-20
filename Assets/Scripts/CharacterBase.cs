35387e36f2d4ba537779294adadb4572a292ad6e
mac duc anh <gumayusi9602@outlook.com>
2024-01-21 00:24:19 +0700

    update

---
+++ Assets/Scripts/CharacterBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts
+{
+    public enum State 
+    {
+        Idle,
+        Dead,
+        Jump,
+        Walk
+    }
+
+    public class CharacterBase : MonoBehaviour
+    {
+        [SerializeField] protected Animator animator;
+        [SerializeField] protected Rigidbody2D rb;
+        [SerializeField] protected float walkSpeed;
+        protected bool _isIdle = true;
+        protected bool _isDead = false;
+        protected bool _isJump = false;
+        protected bool _isWalk = false;
+
+        
+    }
+    
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,45 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace Scripts{
-    
-    public class PlayerController : MonoBehaviour
+namespace Scripts
+{
+
+    public class PlayerController : CharacterBase
     {
+        [SerializeField] CapsuleCollider2D capsuleCollider;
         [SerializeField] Vector2 input;
         [SerializeField] bool isDie = false;
-        private Rigidbody2D rb;
-        [SerializeField] float walkSpeed;
         [SerializeField] float jumpSpeed;
         [SerializeField] bool isHorizontal = false;
-        Animator animator;
-
-        [SerializeField] string currentState;
-        //public string Walk = "Walk";
+        [SerializeField] float yVelocity;
+        [SerializeField] bool IsTouchGround;
+        State currentState;
+        private void Awake()
+        {
+        }
 
         // Start is called before the first frame update
         void Start()
         {
-            rb = GetComponent<Rigidbody2D>();   
-            animator = GetComponent<Animator>();
+
         }
-    
+
         // Update is called once per frame
         void Update()
         {
+            animator.SetFloat("yVelocity", rb.velocity.y);
+            //if (capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+            //{
+            //    if (rb.velocity == Vector2.zero)
+            //    {
+            //        animator.SetBool("Idle", true);
+            //    }
+            //    else
+            //    {
+            //        animator.SetBool("Idle", false);
+            //        Walk();
+            //        FlipFace();
+            //    }
+
+            //}
+            if (capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")) && rb.velocity == Vector2.zero)
+            {
+                animator.SetBool("Idle", _isIdle);
+            }
+            else
+            {
+                _isIdle = false;
+               // animator.SetBool("Idle", false);
+            }
             Walk();
             FlipFace();
-           
-        }
 
-        private void TransitionToState(string nextState)
-        {
-            if (nextState == currentState) { return; }
-            animator.SetBool(nextState, true);
         }
 
         private void OnMove(InputValue value)
         {
-            if(isDie) { return; }
+            if (isDie) { return; }
             input = value.Get<Vector2>();
         }
 
@@ -51,26 +70,46 @@ namespace Scripts{
             isHorizontal = input.x != 0;
             Vector2 myVelocity = new Vector2(input.x * walkSpeed, rb.velocity.y);
             rb.velocity = myVelocity;
+            animator.SetBool("Walk", isHorizontal);
         }
 
         private void OnJump(InputValue value)
         {
-            if(isDie) { return; }   
-            if(value.isPressed)
+            if (isDie) { return; }
+            if (value.isPressed && capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
             {
-                rb.velocity = new Vector2(0,jumpSpeed);
+                rb.velocity = new Vector2(0, jumpSpeed);
+                animator.SetBool("Jump", true);
             }
         }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Ground"))
+            {
+                IsTouchGround = true;
+                animator.SetBool("Jump", false);
+            }
+        }
+
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Ground"))
+            {
+                IsTouchGround = false;
+            }
+        }
+
 
         private void FlipFace()
         {
             Vector3 velo = transform.localScale;
-            if(isHorizontal)
+            if (isHorizontal)
             {
                 float x = Mathf.Sign(input.x);
-                transform.localScale = new Vector3(x , velo.y , velo.z);
+                transform.localScale = new Vector3(x, velo.y, velo.z);
             }
         }
     }
-    
+
 }
